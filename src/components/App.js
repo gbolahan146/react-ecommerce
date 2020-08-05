2e84ff0cd7167161bcf5aeab390924dc406c1226
Caleb335 <belac335@gmail.com>
2020-08-05 16:14:10 +0100

    add firebase sdk

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import React from 'react';
 
 export default function App() {
-    return (
-        <div className="app__base">
-            <h1>Welcome Here!  😍 </h1>
-        </div>
-    )
+  return (
+    <div className="app__base">
+      <h1>Welcome Here! 😍 </h1>
+    </div>
+  );
 }
