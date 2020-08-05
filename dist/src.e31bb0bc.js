9a3407ead1c912fd544bcdddaa9268bce8087bdc
kodecheff <akunneemeka125@gmail.com>
2020-08-05 13:11:33 +0100

    pull cake images

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28626,7 +28626,8 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37061" + '/');
+
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44309" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
