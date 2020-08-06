ead72c75308c085999bf0b7355f0d9fcab95eafb
Folasayo Akinyosoye <41623833+folarmi@users.noreply.github.com>
2020-08-06 09:09:32 +0100

    tried building the product page (#2)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Productpage from './Product_page';
+import Images from './Images'
 
 export default function App() {
   return (
     <div className="app__base">
       <h1>Welcome Here! 😍 </h1>
+      <Productpage Images={Images}/>
     </div>
   );
 }
