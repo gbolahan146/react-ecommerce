94161ea37899d5888a4fca3fda96c78120ce0bc2
kodecheff <akunneemeka125@gmail.com>
2020-08-06 12:47:31 +0100

    dist

--- dist/landing-page.92a2cf24.js
+++ dist/landing-page.92a2cf24.js
@@ -166,7 +166,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "36443" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38675" + '/');
+>>>>>>> dist
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -30939,7 +30939,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36443" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38675" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
