11c661d04ec75d887e9a6f99d0303ed67bdefc03
Caleb335 <belac335@gmail.com>
2020-08-06 09:12:03 +0100

    updates

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,20 +1,19 @@
-import app from 'firebase/app'
+import app from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABAS_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,  
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABAS_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // creating a class instance of firebase depedency/sdk
 export default class Firebase {
-    constructor() {
-        app.initializeApp(firebaseConfig)
-    }
+  constructor() {
+    app.initializeApp(firebaseConfig);
+  }
 }
-
