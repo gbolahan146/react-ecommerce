a273b0b4e98c71ce2ed94f1a31f1d890086022e1
Daniel Beckley <tomiwabeckley@gmail.com>
2020-08-09 21:36:34 +0100

    onChange function and OnSubmit

--- src/pages/Signin-page.js
+++ src/pages/Signin-page.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
-
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (event) {
+        console.log(event.target.value)
+        const { value, name } = event.target;
+        this.setState({ [name]: value})
+    }
+
+    handleSubmit (event)  {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="Register">
@@ -12,20 +31,21 @@ class Login extends Component {
             <div className="Register-form-lyt">
                 <div className="Register-form">
                     <h3 className="Register-heading">Sign In</h3>
-                    <h5 className="Register-subtext">Not into cake yet? <span>Get In!</span></h5>
-                    <form className="Reg-form Login-form">
+                    <h5 className="Register-subtext">Not into cake yet? <span onClick={() => history.push('/signup')}>Get In!</span></h5>
+                    <form onSubmit={this.handleSubmit} className="Reg-form Login-form">
                         <div>
                             <label>
-                                <input type="email" placeholder="Email Address" className="input-full"/>
+                                <input onChange={this.handleChange} name='email' type="email" placeholder="Email Address" className="input-full"/>
                             </label>
                         </div>
                         <div>
                             <label>
-                                <input type="password" placeholder="Password" className="input-full"/>
+                                <input onChange={this.handleChange} name='password' type="password" placeholder="Password" className="input-full"/>
                             </label>
                         </div>
                         <button className="Register-btn">Login</button>
                     </form>
+                    <button className="Register-btn">Sign in With Google</button>
                 </div>
             </div>
         </div>
