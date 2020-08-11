67d24d40daeeacf6ed0f17b04506d550b8175b13
kelvin <osekelvin22@gmail.com>
2020-08-11 10:11:11 +0100

    added firebase config files

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -2,6 +2,16 @@ import firebase from 'firebase';
 import 'firefabse/auth';
 import 'firebase/firestore';
 
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.REACT_APP_MESSENGER_ID,
+    appId: process.REACT_APP_APP_ID
+};
+firebase.initializeApp(firebaseConfig);
 
 
 
