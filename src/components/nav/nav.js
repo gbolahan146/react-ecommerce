26630035fe1b7dee32b3f9495f289d639c2f5df5
cyber-mat <olaniyanolamide@gmail.com>
2020-08-12 20:19:32 +0100

    Landing page responsiveness

---
+++ src/components/nav/nav.js
@@ -0,0 +1,15 @@
+// import React, { useRef } from 'react';
+// class Li extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <li className='nav-item'>{this.props.children}</li>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Li;
