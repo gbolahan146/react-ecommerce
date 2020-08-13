b0bffe7bf5beec1af15eec9b11c526eb31bb5e01
Peter Olu <32315813+pemeraldy@users.noreply.github.com>
2020-08-13 18:15:35 +0100

    added cupcakes section to products page (#47)

--- src/pages/product-page/index.js
+++ src/pages/product-page/index.js
@@ -17,19 +17,14 @@ import cup4 from '../../assets/img/cakes/cup/cup4.jpg';
 const ProductPage = () => {
     return (
         <div className="product_page">
-            <header>
-                <div>
-                    <h1>Logo</h1>
-                </div>
-                <input type="text" placeholder="Search for a cake.." />
-                <div>
-                    <li>Contacts</li>
-                    <li>Help</li>
-                </div>
-            </header>
+            <header></header>
+
+
+            
 
             <div className="banner">
                 <img src="" alt="" />
+
 
                 <div>
                     <p>The Best Cakes in Every Category</p>
@@ -40,6 +35,177 @@ const ProductPage = () => {
             <div className="image-gallery">
                 <div className="row">
                     <h2>Birthday Cakes</h2>
+
+                    <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                        <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                            <div className='px-5 pt-5'>
+                                <img src={birthday} className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title">cake 1</h1>
+                                <button href="#" class="btn btn-danger">Add to the card</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                        <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                            <div className='px-5 pt-5'>
+                                <img src={birthday1} className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title">cake 2</h1>
+                                <button href="#" className="btn btn-danger">Add to the card</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                        <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                            <div className='px-5 pt-5'>
+                                <img src={birthday2} className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title">cake 3</h1>
+                                <button href="#" className="btn btn-danger">Add to the card</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                        <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                            <div className='px-5 pt-5'>
+                                <img src={birthday3} className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title">cake 4</h1>
+                                <button href="#" className="btn btn-danger">Add to the card</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                        <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                            <div className='px-5 pt-5'>
+                                <img src={birthday4} className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title">cake 5</h1>
+                                <button href="#" className="btn btn-danger">Add to the card</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                        <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                            <div className='px-5 pt-5'>
+                                <img src={birthday5} className="card-img-top img-fluid" />
+                            </div>
+                            <div className="card-body">
+                                <h1 className="card-title">cake 6</h1>
+                                <button href="#" className="btn btn-danger">Add to the card</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+
+                <div className="cupcakes">
+                    <div className="row">
+                        <h2>Cup Cakes</h2>
+                        <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                            <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                                <div className='px-5 pt-5'>
+                                    <img src={birthday} className="card-img-top img-fluid" />
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title">cake 1</h1>
+                                    <button href="#" class="btn btn-danger">Add to the card</button>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                            <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                                <div className='px-5 pt-5'>
+                                    <img src={cup} className="card-img-top img-fluid" />
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title">Cup cake</h1>
+                                    <button href="#" className="btn btn-danger">Add to the card</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                            <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                                <div className='px-5 pt-5'>
+                                    <img src={cup1} className="card-img-top img-fluid" />
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title">Cup cake</h1>
+                                    <button href="#" className="btn btn-danger">Add to the card</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                            <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                                <div className='px-5 pt-5'>
+                                    <img src={cup3} className="card-img-top img-fluid" />
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title">Cup cake</h1>
+                                    <button href="#" className="btn btn-danger">Add to the card</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                            <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                                <div className='px-5 pt-5'>
+                                    <img src={cup2} className="card-img-top img-fluid" />
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title">Cup cake</h1>
+                                    <button href="#" className="btn btn-danger">Add to the card</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className=" column col-lg-3 col-md-4 col-sm-6" style={{ borderRadius: "50%" }}>
+                            <div className="card text-center shadow-lg " style={{ marginTop: "25px", marginBottom: "25px", borderRadius: "5%" }}>
+                                <div className='px-5 pt-5'>
+                                    <img src={cup4} className="card-img-top img-fluid" />
+                                </div>
+                                <div className="card-body">
+                                    <h1 className="card-title">Cup cake</h1>
+                                    <button href="#" className="btn btn-danger">Add to the card</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
                         <div className=" column col-lg-3 col-md-4 col-sm-6"  style={{borderRadius: "50%"}}>
                             <div className="card text-center shadow-lg " style={{marginTop: "25px", marginBottom: "25px", borderRadius: "5%"}}>
                                 <div className='px-5 pt-5'>
@@ -129,23 +295,10 @@ const ProductPage = () => {
 
                 <div></div>
             </div>
+
         </div>
     )
 }
 
 export default ProductPage;
 
-// {
-//   Images[0].map(image => (
-//     <img key={image.id} src={image.src} style={{
-//           width:'400px'
-//         }}/>
-//   ))
-// }
-// {
-//   Images[1].map(image => (
-//     <img key={image.id} src={image.src} style={{
-//           width:'400px'
-//         }}/>
-//   ))
-// }
